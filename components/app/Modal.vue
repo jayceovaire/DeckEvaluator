d8ed8a4cadbeff166868d4276e75ce75d6b877ae
jayceovaire <118041687+jayceovaire@users.noreply.github.com>
2025-07-21 18:16:34 -0400

    Modularized power calculator and added modal for the page

---
+++ components/app/Modal.vue
@@ -0,0 +1,36 @@
+<template><UModal
+
+    title="Choose the format of your import"
+    :close="{
+      color: 'primary',
+      variant: 'outline',
+      class: 'rounded-full'
+    }"
+>
+<UButton class="h-12 w-52 cursor-pointer"
+         label="Import" color="primary"
+>
+  Import * Moxfield * Arkidekt
+</UButton>
+<template #body>
+  <UForm class="w-full">
+    <UTextarea
+
+        placeholder="Paste your decklist here..."
+        class="w-full"
+        style="resize: none;"
+        :rows="15"
+
+
+    />
+    <UButton type="submit" class="h-12 w-52 mt-5 cursor-pointer">Submit</UButton>
+  </UForm>
+
+</template>
+</UModal>
+
+</template>
+
+<script setup lang="ts">
+const testRef = ref('')
+</script>
--- components/app/PowerCalculator.vue
+++ components/app/PowerCalculator.vue
@@ -2,23 +2,12 @@
 import { ref } from 'vue'
 
 const list = ref('')
+
 </script>
 
 <template>
   <div class="flex flex-col gap-4 items-center w-full max-w-xl p-4  rounded-lg ">
-    <UButton class="h-12 w-52">
-      Import * Moxfield * Arkidekt
-    </UButton>
-
-    <UForm class="w-full">
-      <UTextarea
-          v-model="list"
-          placeholder="Paste your decklist here..."
-          class="w-full h-64"
-          style="resize: none;"
-
-      />
-    </UForm>
+  <appModal/>
   </div>
 </template>
 
